127af43991097713496063a7b7700ae80e8bdbd1
Sebastians Zudie <sezudie@gmail.com>
2026-02-10 10:40:59 +0000

    Store cells in a 2D array

--- Quax/src/QuaxBoard.java
+++ Quax/src/QuaxBoard.java
@@ -1,13 +1,13 @@
 public class QuaxBoard {
 
 
-    public static void displayBoard(){
+    public static void displayBoard(/* boardState */){
 
         // we will pass the state of the board into this function which is
         // a list of cells. each cell will have a label (X, O, B?, W?) to display
-        // so like when the cell is occupied by WHITE it dissplays W
+        // so like when the cell is occupied by WHITE it displays W
         // Here now X is an octagonal cells and O is rhombic
-
+        // boardState will return a list of cells that we will print here
 
         // print letters
         System.out.print("      ");
@@ -41,6 +41,28 @@ public class QuaxBoard {
                     if (k != 9){
                         System.out.print("-  ");
                     }
+                }
+            }
+            System.out.println();
+        }
+
+        // storing chars in boardState im a 2D array
+        // have to change to list?
+        char[][] boardState = new char[21][21];
+        for (int i = 0; i < 21; i++){
+            for (int j = 0; j < 11; j++){
+                if (i % 2 == 0) {
+                    boardState[i][j] = 'X';
+                } else if (j != 10) {
+                    boardState[i][j] = 'O';
+                }
+            }
+        }
+
+        for (int i =  0; i < 21; i++){
+            for (int j = 0; j < 11; j++){
+                if (j < 10 || i % 2 == 0) {
+                    System.out.print(boardState[i][j]);
                 }
             }
             System.out.println();
